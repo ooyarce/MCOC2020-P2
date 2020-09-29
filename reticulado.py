4496e09b4b84cdbe312303be1a7adb6bc2432165
exequielvial <ejvial@miuandes.cl>
2020-09-28 22:42:41 -0300

    Create reticulado.py

---
+++ reticulado.py
@@ -0,0 +1,36 @@
+import numpy as np 
+
+class Reticulado(object): 
+    def __init__(self):
+        super(Reticulado, self).__init__()
+        self.xyz = np.zeros((0,3), dtype=np.double)
+        self.Nnodos = 0
+        self.barras = []
+        self.cargas = {}
+        self.restricciones = {}
+        
+    def agregar_nodo(self, x, y, z=0):
+        self.xyz.resize((self.Nnodos+1,3))
+        self.xyz[self.Nnodos,:] = [x,y,z]
+        self.Nnodos +=1
+        return
+        
+    def agregar_barra(self,barra):
+        self.barras.append(barra)
+        
+    def obtener_coordenada_nodal(self, n):
+        if n >= self.Nnodos:
+            return
+        return self.xyz[n, :]
+    
+    def calcular_peso_total(self):
+        peso = 0.
+        for b in self.barras:
+            peso += b.calcular_peso(self)
+        return peso
+    
+    def obtener_nodos(self):
+        return self.xyz[0: self.Nnodos,:].copy()
+    
+    def obtener_barras(self):
+        return self.barras
